7dceb2ba3aa2d92f61ce9f5cc1db7a6b96c4da86
Eleanor Chinn <chinnep@miamioh.edu>
2022-12-09 14:46:03 -0500

    update day 4 solution

--- day-4/duplicateCleanup.js
+++ day-4/duplicateCleanup.js
@@ -6,36 +6,24 @@ exports = module.exports = (file, hasAnyOverlap = false) => {
     .split('\n')
     .map(el => el.replace('\r', ''))
 
-  const part1 = input
-    .map(pair => {
-      const [elfA, elfB] = pair.split(',')
-      const [minA, maxA] = elfA.split('-').map(val => Number(val))
-      const [minB, maxB] = elfB.split('-').map(val => Number(val))
+  const result = input.map(pair => {
+    const [elfA, elfB] = pair.split(',')
+    const [minA, maxA] = elfA.split('-').map(val => Number(val))
+    const [minB, maxB] = elfB.split('-').map(val => Number(val))
 
-      if ((minA <= minB && maxA >= maxB) || (minB <= minA && maxB >= maxA)) return 1
+    // One section is completely encompassed by another
+    if ((minA <= minB && maxA >= maxB) || (minB <= minA && maxB >= maxA)) return 1
 
-      return 0
-    })
-    .reduce((sum, isDuplicateWork) => sum + isDuplicateWork, 0)
-
-    const part2 = input
-    .map(pair => {
-      const [elfA, elfB] = pair.split(',')
-      const [minA, maxA] = elfA.split('-').map(val => Number(val))
-      const [minB, maxB] = elfB.split('-').map(val => Number(val))
-
-      // One section is completely encompassed by another
-      if ((minA <= minB && maxA >= maxB) || (minB <= minA && maxB >= maxA)) return 1
-
+    if (hasAnyOverlap) {
       // Overlap on left side
       if ((minA <= minB && maxA >= minB) || (minB <= minA && maxB >= minA)) return 1
 
       // Overlap on right side
       if ((minA >= minB && minA <= maxB) || (minB >= minA && minB <= maxA)) return 1
-  
-      return 0
-    })
-    .reduce((sum, isDuplicateWork) => sum + isDuplicateWork, 0)
+    }
 
-  return hasAnyOverlap? part2 : part1
+    return 0
+  }).reduce((sum, isDuplicateWork) => sum + isDuplicateWork, 0)
+
+  return result
 }
